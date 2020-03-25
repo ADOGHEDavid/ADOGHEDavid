cecaeb8ecc3c8226e51aac5c79ec2c0ce10fb8d7
Adoghe David <36283065+ADOGHEDavid@users.noreply.github.com>
2020-03-25 19:45:05 +0100

    Add files via upload

---
+++ PyCalculator.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 16:52:38 2020
+
+@author: davidad
+"""
+PI = 3.14
+radius = float(input('Enter the radius of the Circle : '))
+Area_of_Circle = PI * radius * radius
+
+print ('the area of the Circle is ' +   str(Area_of_Circle))
